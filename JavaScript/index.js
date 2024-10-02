f3797ce90cd787aa6ec459cb1d33b1a07a954313
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2024-10-02 15:32:35 +0900

    フッターを変えた

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -131,7 +131,7 @@ async function createEmbed(postData) {
         { name: '検知地域', value: postData.Regions.join(', '), inline: true }
     );
 
-    embed.setFooter({ text: `イベントID: ${postData.EventId}\n検知ID: ${postData.KyoshinEventId}` });
+    embed.setFooter({ text: '強震モニタ' });
 
     const attachment = await captureScreenshot();
 
